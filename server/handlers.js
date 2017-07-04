788713d89a1e941e00932dfa108e9b407a6ce54d
Lindsay Anchors <lindsay.anchors@gmail.com>
2017-07-04 12:17:51 -0700

    Setup POST handler

--- server/handlers.js
+++ server/handlers.js
@@ -1,7 +1,23 @@
 const db = require('./models/index.js');
 const Task = db.Task;
 
-module.exports.taskHandler = function taskHandler(req, res) {
-  Task.findAll()
-  .then(tasks => res.send(tasks));
+const taskHandler = (req, res) => {
+  switch (req.method) {
+  case 'GET':
+    Task.findAll()
+    .then(tasks => res.send(tasks))
+    .catch(err => console.log('ERROR getting all tasks: ', err));
+    break;
+  case 'POST':
+    Task.create(req.body)
+    .then(createdTask => res.send(createdTask))
+    .catch(err => console.log('ERROR POSTING: ', err));
+    break;
+  default:
+    break;
+  }
+};
+
+module.exports = {
+  taskHandler,
 };
