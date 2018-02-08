23dfdf5d3501100527d0c8257b66bdea123e6606
Lindsay Anchors <lindsay.anchors@gmail.com>
2018-02-07 23:01:29 -0800

    Use env vars for DB config

--- server/models/index.js
+++ server/models/index.js
@@ -8,7 +8,10 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  let prefix = config.use_env_variable
+  sequelize = new Sequelize(
+    process.env[`${prefix}NAME`], process.env[`${prefix}USER`], process.env[`${prefix}PASSWORD`], config
+  );
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').load();
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
