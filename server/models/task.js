64bd3ff887d2d095e3a918fc1ed878776896f637
Lindsay Anchors <lindsay.anchors@gmail.com>
2018-02-11 16:40:21 -0800

    Update model with new datatype

--- server/models/task.js
+++ server/models/task.js
@@ -9,8 +9,8 @@ module.exports = function(sequelize, DataTypes) {
     description: DataTypes.STRING,
     position: DataTypes.STRING, // e.g. "[0, 0]" or "[50, 100]"
     notes: DataTypes.STRING,
-    estimatedTime: { // 00:15, 1:30, etc
-      type: DataTypes.STRING,
+    estimatedTime: { // in minutes
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
     estimateConfidence: {
