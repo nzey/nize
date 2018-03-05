6bb186e9f50de54ff7c448b35c391ce33306544b
Lindsay Anchors <lindsay.anchors@gmail.com>
2018-03-04 16:12:04 -0800

    Clean-up formatting

--- src/app/components/common/breadcrumb/Breadcrumb.jsx
+++ src/app/components/common/breadcrumb/Breadcrumb.jsx
@@ -5,8 +5,11 @@ const Crumb = ({ crumb, index, last, handleClick }) => {
   const isActive = index === last;
   return (
     <div className='crumb-container' key={crumb.id}>
-      <div className={isActive ? 'crumb active' : 'crumb link'} onClick={() => isActive ? null : handleClick(crumb)} >{crumb.title}</div>
-      { index == last ? null : <div className='divider'>></div>}
+      <div
+        className={isActive ? 'crumb active' : 'crumb link'}
+        onClick={isActive ? null : (() => handleClick(crumb))}
+      >{crumb.title}</div>
+      {index === last ? null : <div className='divider'>></div>}
     </div>);
 };
 
@@ -20,7 +23,12 @@ Crumb.propTypes = {
 const Breadcrumb = ({ crumbs, handleClick }) => {
   return (
     <div className='breadcrumb'>
-      {crumbs.map((crumb, index, array) => <Crumb crumb={crumb} index={index} last={array.length - 1} handleClick={handleClick} />)}
+      {crumbs.map((crumb, index, array) => (<Crumb
+        key={crumb.id}
+        crumb={crumb} index={index}
+        last={array.length - 1}
+        handleClick={handleClick}
+      />))}
     </div>
   );
 };
