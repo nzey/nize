8d3d7acd8f3c4bccfc71908622a30d54bc26c6c7
Lindsay Anchors <lindsay.anchors@gmail.com>
2018-03-06 21:23:46 -0800

    Use redux compose

--- src/app/containers/App.jsx
+++ src/app/containers/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { bindActionCreators, compose } from 'redux';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 import Header from '../components/common/Header';
@@ -38,7 +38,7 @@ function mapDispatchToProps(dispatch) {
   }, dispatch);
 }
 
-export default DragDropContext(HTML5Backend)(connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(App));
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  DragDropContext(HTML5Backend)
+)(App);
